ab3dd8bc30db4a4b204fac4d68c1623b15148212
Christian <christianyip92@gmail.com>
2022-09-15 14:49:09 -0700

    Add tests for sunk and hit location.

--- src/ship.test.js
+++ src/ship.test.js
@@ -5,3 +5,15 @@ test('ship gets hit at 0', () => {
   expect(Ship.newShip.spaces[0]).toBe(true);
   expect(Ship.newShip.spaces[1]).toBe(false);
 });
+
+test('ship sinks', () => {
+  Ship.newShip.hit(0);
+  Ship.newShip.hit(1);
+  Ship.newShip.hit(2);
+  expect(Ship.newShip.sunk).toBe(true);
+});
+
+test('ship hit location is given after hit', () => {
+  Ship.newShip.hit(0);
+  expect(Ship.newShip.hitLocation).toBe(0);
+});
