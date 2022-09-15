7ecefeed1e4acb5ecf0b4b49ec2364ca9dce06ad
Christian <christianyip92@gmail.com>
2022-09-15 14:49:35 -0700

    Add functions for sunk and hit location.

--- src/ship-factory.js
+++ src/ship-factory.js
@@ -1,24 +1,29 @@
 class Ship {
   constructor(length) {
     this.length = length;
-    this.health = 3;
+    this.health = length;
     this.hitLocation = null;
     this.spaces = {};
     this.sunk = false;
     this.createSpaces(length);
   }
 
+  // takes a number and then marks that space as ‘hit’
   hit(n) {
-    // takes a number and then marks that space as ‘hit’
     this.spaces[n] = true;
+    this.hitLocation = n;
+    this.health--;
+    this.isSunk();
   }
 
+  // changes sink value
   isSunk() {
     if (this.health === 0) {
       this.sunk = true;
     }
   }
 
+  // creates spaces that can be hit
   createSpaces(length) {
     for (let i = 0; i < length; i++) {
       this.spaces[i] = false;
